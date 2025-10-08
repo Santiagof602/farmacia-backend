d5f87a39974d191c8fcdb4bc460279d895b7e295
MarceloEscobal <marcelo.escobal@anima.edu.uy>
2025-10-08 16:19:11 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -11,26 +11,66 @@ class User extends Model {
         },
         firstname: {
           type: DataTypes.STRING,
+          allowNull: false,
         },
         lastname: {
           type: DataTypes.STRING,
+          allowNull: false,
         },
-        Email: {
+        email: {
           type: DataTypes.STRING,
+          unique: true,
+          allowNull: false,
+          validate: {
+            isEmail: true,
+          },
         },
-        Password: {
+      password: {
           type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [6, 100]
+          },
         },
-        Role: {
-          type: DataTypes.SYMBOL,
-        },
-      },
+        role: {
+          type: DataTypes.ENUM('admin', 'user'),
+          defaultValue: 'user',
+          allowNull: false,
+          },
+          
+        }
+      ,
       {
         sequelize,
         modelName: "user", // Nombre del modelo en singular y en minúscula.
-      },
+        paranoid: true, // Habilita el borrado lógico (soft delete).
+        hooks: { 
+          beforeCreate: async (user) => {
+            user.password = user.password.toLowerCase();
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          },
+          beforeUpdate: async (user) => {
+            if (user.changed('password')) {
+              user.password = user.password.toLowerCase();
+              const salt = await bcrypt.genSalt(10);
+              user.password = await bcrypt.hash(user.password, salt); 
+            }
+          }
+        },
+        // timestamps: true, // Habilita las marcas de tiempo (createdAt, updatedAt).
+        // underscored: true, // Usa snake_case para los nombres de las columnas.
+      }
     );
     return User;
+  }
+  toJSON() {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+  }
+  async validatePassword(password) {
+    return await bcrypt.compare(password.toLowerCase(), this.password);
   }
 }
 
